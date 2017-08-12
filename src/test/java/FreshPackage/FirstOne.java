6811f97f70b40211aa02cb6c65d7f272e788b8a7
prem sarkar <premsarkar2222@gmail.com>
2017-08-12 23:51:25 +0530

    new Branch commited

--- src/test/java/FreshPackage/FirstOne.java
+++ src/test/java/FreshPackage/FirstOne.java
@@ -5,6 +5,16 @@ public class FirstOne {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Trying my best Plz GOD help me");
+
+		for(int i=0; i<=10; i++){
+			System.out.println(i);
+
+
+
+		
+		
+		}
+
 	}
 
 }
